f8e80295c6885c720564edb856a89adea70e649d
JiangKlijna <leil729564090@outlook.com>
2016-07-22 23:08:31 +0800

    :boom:

--- FacadePattern/FacadePattern.py
+++ FacadePattern/FacadePattern.py
@@ -1,25 +1,71 @@
 # encoding = utf-8
 # @author jiang
 
-class foo(object):
-    def f1(self):
-        print("original f1")
+import time
+import threading
 
-    def f2(self):
-        print("original f2")
+SLEEP = 0.5
 
-class foo_decorator(object):
-    def __init__(self, decoratee):
-        self._decoratee = decoratee
 
-    def f1(self):
-        print("decorated f1")
-        self._decoratee.f1()
+# Complex Parts
+class TC1(threading.Thread):
+    def __init__(self):
+        super(TC1, self).__init__()
 
-    def __getattr__(self, name):
-        return getattr(self._decoratee, name)
+    def run(self):
+        print("###### In Test 1 ######")
+        time.sleep(SLEEP)
+        print("Setting up")
+        time.sleep(SLEEP)
+        print("Running test")
+        time.sleep(SLEEP)
+        print("Tearing down")
+        time.sleep(SLEEP)
+        print("Test Finished\n")
 
-u = foo()
-v = foo_decorator(u)
-v.f1()
-v.f2()
+
+class TC2(threading.Thread):
+    def __init__(self):
+        super(TC2, self).__init__()
+
+    def run(self):
+        print("###### In Test 2 ######")
+        time.sleep(SLEEP)
+        print("Setting up")
+        time.sleep(SLEEP)
+        print("Running test")
+        time.sleep(SLEEP)
+        print("Tearing down")
+        time.sleep(SLEEP)
+        print("Test Finished\n")
+
+
+class TC3(threading.Thread):
+    def __init__(self):
+        super(TC3, self).__init__()
+
+    def run(self):
+        print("###### In Test 3 ######")
+        time.sleep(SLEEP)
+        print("Setting up")
+        time.sleep(SLEEP)
+        print("Running test")
+        time.sleep(SLEEP)
+        print("Tearing down")
+        time.sleep(SLEEP)
+        print("Test Finished\n")
+
+
+# Facade
+class TestRunner:
+    def __init__(self):
+        self.tcs = [TC1, TC2, TC3]
+        self.tests = [e() for e in self.tcs]
+
+    def runAll(self):
+        [i.start() for i in self.tests]
+
+# Client
+if __name__ == '__main__':
+    testrunner = TestRunner()
+    testrunner.runAll()
